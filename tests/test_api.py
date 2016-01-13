b1f6de813a65b2f62fe156b43ebb2797b5bff5b5
Aline Manera <alinefm@linux.vnet.ibm.com>
2016-01-13 10:00:38 -0200

    Create /config API to return the Wok server configuration
    The SSL and websockets ports are related to the Wok server. So create a
    specific API to care about these information.
    
    During the Wok/Kimchi split, these information was wrongly moved to
    Kimchi plugin, but as it is related to the server it should be on Wok.
    
    Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>

---
+++ tests/test_api.py
@@ -0,0 +1,57 @@
+#
+# Project Wok
+#
+# Copyright IBM, Corp. 2016
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+import json
+import unittest
+from functools import partial
+
+import utils
+
+
+test_server = None
+model = None
+host = None
+port = None
+ssl_port = None
+
+
+def setUpModule():
+    global test_server, model, host, port, ssl_port
+
+    utils.patch_auth()
+    host = '127.0.0.1'
+    port = utils.get_free_port('http')
+    ssl_port = utils.get_free_port('https')
+    test_server = utils.run_server(host, port, ssl_port, test_mode=True)
+
+
+def tearDownModule():
+    test_server.stop()
+
+
+class APITests(unittest.TestCase):
+
+    def setUp(self):
+        self.request = partial(utils.request, host, ssl_port)
+
+    def test_config(self):
+        resp = self.request('/config').read()
+        conf = json.loads(resp)
+        keys = ["ssl_port", "websockets_port", "version"]
+        self.assertEquals(sorted(keys), sorted(conf.keys()))

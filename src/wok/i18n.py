222c4c6b5643c7be28729a3e4952ab942dadbd51
Lucio Correia <luciojhc@linux.vnet.ibm.com>
2016-06-09 17:59:04 -0300

    Revert "Use past verbs"
    This reverts commit 04358829a34f246efa7198191b4ca7a851c02bbb.

--- src/wok/i18n.py
+++ src/wok/i18n.py
@@ -58,6 +58,6 @@ messages = {
     # These messages (ending with L) are for user log purposes
     "WOKCOL0001L": _("Request made on collection"),
     "WOKRES0001L": _("Request made on resource"),
-    "WOKROOT0001L": _("User '%(username)s' logged in"),
-    "WOKROOT0002L": _("User '%(username)s' logged out"),
+    "WOKROOT0001L": _("User '%(username)s' login"),
+    "WOKROOT0002L": _("User '%(username)s' logout"),
 }

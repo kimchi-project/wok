kimchi-project/wok

commit 2a7b3c610826db9ff0b9d6d0549220798b8a77b6

Issue #139: Do not generate logrotate config file on the fly
Signed-off-by: Ramon Medeiros <ramonn@linux.vnet.ibm.com>

src/Makefile.am
@@ -24,6 +24,7 @@ SUBDIRS = wok nginx
 EXTRA_DIST = wokd.in \
 	wok.conf.in \
 	firewalld.xml \
+	wok.logrotate \
 	$(NULL)
 
 bin_SCRIPTS = wokd

src/wok/server.py
@@ -30,7 +30,7 @@ from string import Template
 from wok import auth
 from wok import config
 from wok.config import config as configParser
-from wok.config import paths, PluginConfig, WokConfig
+from wok.config import PluginConfig, WokConfig
 from wok.control import sub_nodes
 from wok.model import model
 from wok.proxy import start_proxy
@@ -45,17 +45,6 @@ LOGGING_LEVEL = {"debug": logging.DEBUG,
                  "warning": logging.WARNING,
                  "error": logging.ERROR,
                  "critical": logging.CRITICAL}
-LOGROTATE_TEMPLATE = """
-${log_dir}/*log {
-    daily
-    nomail
-    maxsize ${log_size}
-    rotate 10
-    nomissingok
-    compress
-}
-"""
-
 
 def set_no_cache():
     from time import strftime, gmtime
@@ -146,21 +135,6 @@ class Server(object):
 
         # start request logger
         self.reqLogger = RequestLogger()
-
-        # only add logrotate if wok is installed
-        if paths.installed:
-
-            # redefine logrotate configuration according to wok.conf
-            data = Template(LOGROTATE_TEMPLATE)
-            data = data.safe_substitute(
-                log_dir=configParser.get("logging", "log_dir"),
-                log_size=configParser.get("logging", "log_size")
-            )
-
-            # Write file to be used for nginx.
-            config_file = open(os.path.join(paths.logrotate_dir, "wokd"), "w")
-            config_file.write(data)
-            config_file.close()
 
         # Handling running mode
         if not dev_env:

Makefile.am
@@ -155,7 +155,7 @@ install-data-local:
 	$(INSTALL_DATA) src/dhparams.pem $(DESTDIR)/etc/wok/dhparams.pem
 	touch $(DESTDIR)/etc/nginx/conf.d/wok.conf
 	mkdir -p $(DESTDIR)/etc/logrotate.d/
-	touch $(DESTDIR)/etc/logrotate.d/wokd
+	$(INSTALL_DATA) $(top_srcdir)/src/wok.logrotate $(DESTDIR)/etc/logrotate.d/wokd
 
 uninstall-local:
 	@if test -f $(systemdsystemunitdir)/wokd.service; then \

src/wok.conf.in
@@ -50,13 +50,15 @@
 
 [logging]
 # Log directory
+
+# Be aware that editting logging directory, you must change logrotate
+# configuration file to keep logs being compressed.
+# Look /etc/logrotate.d/wokd
+
 #log_dir = @localstatedir@/log/wok
 
 # Logging level: debug, info, warning, error or critical
 #log_level = debug
-
-# Log file size (specify size in K, M or G)
-#log_size = 10M
 
 [authentication]
 # Authentication method, available option: pam, ldap.

src/wok.logrotate
@@ -1,0 +1,9 @@
+/var/log/wok {
+    daily
+    nomail
+    maxsize 10M
+    rotate 10
+    nomissingok
+    compress
+}
+

commit 232dbdf7a6bf3bd1648bff40273952061ff63795

Fix pep8 issues caused by 2a7b3c6
Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>

IBM-license-blacklist
@@ -28,6 +28,7 @@ po/Makevars
 po/POTFILES.in
 po/wok.pot
 src/firewalld.xml
+src/wok.logrotate
 src/wok.conf.in
 src/wok/API.json
 src/wok/plugins/sample/API.json

src/Makefile.am
@@ -1,7 +1,7 @@
 #
 # Project Wok
 #
-# Copyright IBM Corp, 2013-2015
+# Copyright IBM Corp, 2013-2016
 #
 # Code derived from Project Kimchi
 #

src/wok/server.py
@@ -25,8 +25,6 @@ import logging
 import logging.handlers
 import os
 
-from string import Template
-
 from wok import auth
 from wok import config
 from wok.config import config as configParser
@@ -45,6 +43,7 @@ LOGGING_LEVEL = {"debug": logging.DEBUG,
                  "warning": logging.WARNING,
                  "error": logging.ERROR,
                  "critical": logging.CRITICAL}
+
 
 def set_no_cache():
     from time import strftime, gmtime
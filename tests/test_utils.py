844775dfb27724f725bef981801223e4c24bd5b1
Aline Manera <aline.manera@gmail.com>
2017-01-11 14:09:06 -0200

    Fix typo in license header
    Some files were using "Code delivered from Kimchi project" instead of
    "Code derived from Kimchi project". So fix it.
    
    Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,9 @@
 #
 # Project Wok
 #
-# Copyright IBM Corp, 2015-2016
+# Copyright IBM Corp, 2015-2017
 #
-# Code delivered from Project Kimchi
+# Code derived from Project Kimchi
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public

5864c7baa5426ad040934dff3adc9e77d2ec5f74
Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>
2017-01-24 11:19:44 -0200

    reload API: control and model changes
    - added a new action handler called 'reload' in control/config.py
    - added a method 'reload' in model/config.py that calls
    'cherrypy.engine.restart()'.
    
    Signed-off-by: Daniel Henrique Barboza <danielhb@linux.vnet.ibm.com>

--- src/wok/model/config.py
+++ src/wok/model/config.py
@@ -1,7 +1,7 @@
 #
 # Project Wok
 #
-# Copyright IBM Corp, 2016
+# Copyright IBM Corp, 2016-2017
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,6 +17,8 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
+import cherrypy
+
 from wok.config import config, get_version
 
 
@@ -30,3 +32,6 @@ class ConfigModel(object):
                 'auth': config.get('authentication', 'method'),
                 'server_root': config.get('server', 'server_root'),
                 'version': get_version()}
+
+    def reload(self, name):
+        cherrypy.engine.restart()
